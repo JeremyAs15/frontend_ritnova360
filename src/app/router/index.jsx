8ac31256d5474a9674cb6126c4d38d45fcdbf935
Santiago Guerrero <santiago.david.guerrero@correounivalle.edu.co>
2026-06-06 16:57:16 -0500

    feat: add SignUpPageWrapper

--- src/app/router/index.jsx
+++ src/app/router/index.jsx
@@ -1,6 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage/LoginPage';
 import SignUpPage from '../pages/SignUpPage/SignUpPage';
+import { useNavigate } from 'react-router-dom';
+
+function SignUpPageWrapper() {
+  const navigate = useNavigate();
+  return (
+    <SignUpPage 
+      onLogin={() => navigate('/')} 
+      onSignUp={() => navigate('/')} 
+    />
+  );
+}
 
 function Router() {
   return (
