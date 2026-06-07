2176372a72e6cb445dc38ebac47daa34856cf4c6
NicolleP16 <nicolle.paz@correounivalle.edu.co>
2026-06-07 00:25:01 -0500

    style: import global stylesheet files

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './app/styles/variables.css'
+import './app/styles/forms.css'
 import './index.css'
 import App from './App.jsx'
 
