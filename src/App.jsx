7afba69bb6ae156a25e19b32fcc227c2e1ef4d3d
JP <juanpabloeskmilla@gmail.com>
2026-07-13 14:50:50 -0500

    feat: wrap application with CartProvider in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Analytics } from '@vercel/analytics/react';
 import Router from './app/router/index';
+import { CartProvider } from './app/context/CartContext';
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <Router />
       <Analytics />
-    </>
+    </CartProvider>
   );
 }
 
